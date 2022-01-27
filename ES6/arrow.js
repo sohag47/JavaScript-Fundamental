697d5822a9a2f511ee25cace95541f3f7b8ff0c9
Minhazul Islam Sohag <39290132+sohag47@users.noreply.github.com>
2022-01-27 11:27:41 +0600

    arrow function implement

--- ES6/arrow.js
+++ ES6/arrow.js
@@ -2,29 +2,48 @@
 //! Javascript arrow function 
 
 //? Normal function
-function Test(text){
+function Test ( text ) {
     return text;
 }
-console.log(Test("Hello Arrow"));
+// console.log( Test( "Hello Arrow" ) );
 
 //? normal function keep variable
-let normal = function Test2(text){
+let normal = function Test2 ( text ) {
     return text;
 }
-console.log(normal("Hello Function"));
+// console.log( normal( "Hello Function" ) );
 
 //? short function
-let short_function = function (text){
+let short_function = function ( text ) {
     return text;
 }
-console.log(short_function("Short Function"));
+// console.log( short_function( "Short Function" ) );
 
 //! Arrow Function
-let Arrow_function = (text) => {
+let Arrow_function = ( text ) => {
     return text;
 }
-console.log(Arrow_function("Arrow Function"));
+// console.log( Arrow_function( "Arrow Function" ) );
 
 //? Arrow function short format [only use when parameter only one]
-let arrow_short = text => text;
-console.log(arrow_short("Arrow Short function"));
+let arrow_short = ( text ) => text;
+// console.log( arrow_short( "Arrow Short function" ) );
+
+
+var skills = {
+    name: "JavaScript",
+    libraries: ['React', 'Angular', 'Vue'],
+    printLibraries: function () {
+        //? normal way
+        // var self = this;
+        // console.log( self.name );
+        // this.libraries.forEach( function ( a ) {
+        //     console.log( `${self.name} loves ${a}` );
+        // } )
+
+        //? arrow function way
+        this.libraries.forEach( ( a ) => console.log( `${this.name} Loves ${a}` ) )
+    }
+}
+skills.printLibraries();
+
