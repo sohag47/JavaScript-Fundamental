34ad0874ab385583cba842ded38c47cc331c9524
Minhazul Islam Sohag <39290132+sohag47@users.noreply.github.com>
2022-01-27 12:39:49 +0600

    array method

--- Array/array_sort.js
+++ Array/array_sort.js
@@ -1,7 +1,23 @@
 //! array
-let numbers = [1,2,3,4,5,6,7,8,9,10];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-//? array find()
-let result = numbers.find( (currentValue) => currentValue > 4);
-console.log(result)
+//? array find(currentValue, currentIndex, array)
+var result = numbers.find( ( currentValue ) => currentValue > 4 );
+console.log( "🚀 ~ file: array_sort.js ~ line 7 ~ result", result );
+
+//?array findindex(currentValue, currentIndex, array)
+var result = numbers.findIndex( ( currentValue, currentIndex, array ) => currentValue > 4 );
+console.log( "🚀 ~ file: array_sort.js ~ line 11 ~ result", result );
+
+//?  array  filter()
+var result = numbers.filter( ( currentValue, currentIndex, array ) => currentValue > 4 );
+console.log( "🚀 ~ file: array_sort.js ~ line 15 ~ result", result );
+
+//? array slice
+
+var result = numbers.slice( 1, 3 );
+console.log( "🚀 ~ file: array_sort.js ~ line 20 ~ result", result );
+
+var result = numbers.splice( 1, 2, 10, 12, 13, 19 );
+console.log( "🚀 ~ file: array_sort.js ~ line 23 ~ result", numbers );
