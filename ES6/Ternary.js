b4bb45fe61d021e2fe8de5ae6de5f01a534a7e4c
Minhazul Islam Sohag <39290132+sohag47@users.noreply.github.com>
2022-01-27 11:58:03 +0600

    ternary operator

--- ES6/Ternary.js
+++ ES6/Ternary.js
@@ -1,12 +1,24 @@
-//! Ternary Operator
-let age = 12;
+//!  normal condition
+var age = 11;
+var a = 5;
+if ( age >= 18 ) {
+    console.log( "Adult" );
+} else {
+    if ( age < 10 ) {
+        console.log( "Child" );
+    } else {
+        console.log( "Young" );
+    }
+    console.log( "Child" );
+}
 
-(age >= 18) 
-?
-(console.log("adult"))
-:
-(age < 10) 
-? 
-(console.log("child"))
-:
-(console.log("Teen"))
+//! ternary way:
+var result = ( age >= 18 ) ? "Adult" : ( age < 10 ) ? "Child" : "Young";
+console.log( result );
+
+
+var result = ( a > 5 ) ? true : false;
+console.log( result );
+
+
+console.log( ( a > 5 ) );
